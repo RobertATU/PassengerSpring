460995f47a7f5bf8b69b87fce38278454db6a908
Robert <g00395612@atu.ie>
2022-12-03 12:50:58 +0000

    First Push

---
+++ src/main/java/ie/atu/PassengerSpring/PassengerSpringApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.PassengerSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassengerSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ie/atu/PassengerSpring/PassengerSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.PassengerSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassengerSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
