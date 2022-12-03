d0391873dd8765b91e4f09e3c8ba005dae6b7b78
Robert <g00395612@atu.ie>
2022-12-03 14:01:06 +0000

    Added mapping

--- src/main/java/ie/atu/PassengerSpring/PassengerSpringApplication.java
+++ src/main/java/ie/atu/PassengerSpring/PassengerSpringApplication.java
@@ -3,6 +3,7 @@ package ie.atu.PassengerSpring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +22,18 @@ public class PassengerSpringApplication {
 	public List<Passenger> getpassengers()
 	{
 		List<Passenger> myPassengers = List.of(
-				new Passenger("Mr", "Paul", 1234567890, 23456789, 23),
-				new Passenger("Mr", "Paul2", 1234567890, 23456789, 93),
-				new Passenger("Mr", "Paul3", 1234567890, 23456789, 53));
+				new Passenger("Mr", "Paul", "1234567890", 23456789, 23),
+				new Passenger("Mr", "Paul2", "1234567890", 23876589, 93),
+				new Passenger("Mr", "Paul3", "1234567890", 23456789, 53));
 		return myPassengers;
+
+
+	}
+
+	@GetMapping("/{passengerID}")
+	public Passenger getPassenger(@PathVariable String passengerID)
+	{
+		Passenger myPassenger = new Passenger("Mr", "Paul2", "1234567890", 23876589, 93);
+		return myPassenger;
 	}
 }
